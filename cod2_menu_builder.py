c641859d7e804fa9b8dd19e8dace2011bca7225c
stevomitric <stevo.m00@hotmail.rs>
2018-06-21 18:21:11 +0200

    Imported everything and made the Mian class

---
+++ cod2_menu_builder.py
@@ -0,0 +1,46 @@
+'''       __ ___________            
+  _______/  |\_   _____/__  ______  
+ /  ___/\   __\    __)_\  \/ /  _ \ 
+ \___ \  |  | |        \\   (  <_> )
+/____  > |__|/_______  / \_/ \____/ 
+     \/              \/             
+
+stevo.mitric@yahoo.com
+
+This code has no licence, feel free to do whatever you want with it.
+'''
+
+from Tkinter 	import *
+from ttk		import *
+from threading	import Thread
+
+
+class Main:
+	def __init__(self):
+		
+		
+		self.GUIDraw()
+		
+		
+		
+	def GUIDraw(self):
+		self.root = Tk()
+		self.root.geometry('800x600')
+		self.root.title('Call of Duty 2 Menu Builder - stEvo')
+		
+		
+		
+		self.root.mainloop()
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+if __name__ == '__main__':
+	Main()
